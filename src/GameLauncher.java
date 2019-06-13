d65c43b6756d8bfb64dbc3b88599f183066b0ffb
Perce <51399656+Spercex@users.noreply.github.com>
2019-06-13 12:06:23 +0200

    Update GameLauncher.java

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -1,8 +1,29 @@
 /**
-* @author BOURDIN M, SCHLOSSER M, LE FEUNTEUN F, KHALIFA Y, BAUSSON T
+* @author BOURDIN M, SCHLOSSER M, LE FEUNTEUN F
 */
 
 public class GameLauncher {
+
+	/**
+	 * This method is a shortcut from System.out.println
+	 * @since openjdk version "11.0.3" 2019-04-16
+	 * @param value - value to print
+	 */
+
+	public static void sop(String value){
+		System.out.println(value);
+	}
+
+	/**
+	 * This method is a shortcut from System.err.println
+	 * @since openjdk version "11.0.3" 2019-04-16
+	 * @param value - value to print
+	 */
+
+	public static void sep(String value){
+		System.err.println(value);
+	}
+
 
 	/**
 	 * Use to start the game
@@ -12,7 +33,14 @@ public class GameLauncher {
 
 	public void main(String[] args) {
 		// TODO - implement GameLauncher.main
-		throw new UnsupportedOperationException();
+		sop("\033[31m\n==== Début DU PROGRAMME Quoridor.java ====\033[0m");
+		sop("\033[32m   /=/SCHLOSSER M, BOURDIN M, LE FEUNTEUN F, INFO 1C2 /=/ \n\033[0m");
+
+		QuoridorN quori = new QuoridorN("../src/configure.txt", "Tee", "Perce");
+
+		sop("\033[31m\n==== Fin DU PROGRAMME Quoridor.java ====\033[0m");
+		sop("\033[32m   /=/SCHLOSSER M, BOURDIN M, LE FEUNTEUN F, INFO 1C2 /=/ \n\033[0m");
+
 	}
 
 }
