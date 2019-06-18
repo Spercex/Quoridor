68e601b9729d534830bd2e5d9b6142bc79dbc627
magic-shefla <51371470+magic-shefla@users.noreply.github.com>
2019-06-18 10:43:43 +0200

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,31 @@
+import Quoridor.model.*;
+
+public class Test{
+
+  public static void main(String[] args){
+    // Board b = new Board();
+    // Player p1 = new Player("Jean",b,TypeCase.P1);
+    // Player p2 = new Player("Jacques",b,TypeCase.P2);
+    //
+    // b.setPlayer(p1,5,5);
+    // b.setPlayer(p2,7,5);
+    //
+    // System.out.println(b.toString());
+    // b.setPlayer(p1,7,7);
+    // b.setFence(4,4,0);
+    // b.setFence(5,4,1);
+    // b.setFence(6,5,0);
+    //
+    // b.setFence(8,4,0);
+    // b.checkMoves(p1);
+    //
+    // System.out.println(b.toString());
+
+
+    Game g = new Game("p1","p2", Mode.HH);
+    g.printBoard();
+    g.start();
+    //b.printBoard();
+  }
+
+}
